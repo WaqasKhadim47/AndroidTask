74c383791b95287da5b0e79967569cb61fdd167b
Waqas Khadim <waqaskhadim@MP17-WaqasK-SSE-515.local>
2022-07-06 17:43:01 +0500

    gridlayout set

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.androidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuAdapter
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidproject.Api.ApiInterface
+import com.google.android.material.internal.ContextUtils.getActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
      val BASE_URL ="https://jsonplaceholder.typicode.com/";
@@ -28,7 +36,33 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerView)
         progressBar = findViewById(R.id.pBar)
-        recyclerView.layoutManager = LinearLayoutManager(this);
+       val mLayoutManager = GridLayoutManager(this@MainActivity, 1)
+        val rdb = findViewById<View>(R.id.radioGrp) as RadioGroup
+
+        rdb.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { radioGroup, i ->
+            val checkedRadioButton = radioGroup?.findViewById(radioGroup.checkedRadioButtonId) as? RadioButton
+            checkedRadioButton?.let {
+
+                if (checkedRadioButton.isChecked)
+                   if(checkedRadioButton.id == R.id.linear){
+                       mLayoutManager.spanCount = 1
+                   }else{
+                       checkedRadioButton.id = R.id.grid
+                       mLayoutManager.spanCount= 2
+                   }
+            }
+
+        })
+//        rdb.setOnClickListener { v ->
+//            val checked = (v as RadioButton).isChecked
+//            // Check which radiobutton was pressed
+//            if (checked) {
+//                // Do your coding
+//            } else {
+//                // Do your coding
+//            }
+//        }
+        recyclerView.layoutManager = mLayoutManager
 
         getPosts()
     }
